d9bb35ec6dd79d621f35e87eeb9340180fc7cdfd
Luan rodrigues <rodrigues.luan20@gmail.com>
2023-03-21 15:51:58 -0300

    feat: created error function

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express, {Application}  from "express";
+import { errorHandler } from "./errors"
 
 const app: Application = express()
 app.use(express.json())
+
+app.use(errorHandler)
 
 export default app
