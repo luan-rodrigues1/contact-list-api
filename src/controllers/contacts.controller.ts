79a9a418132182a32aa1cb4804a7fe1765ffdbeb
Luan rodrigues <rodrigues.luan20@gmail.com>
2023-03-23 12:27:49 -0300

    feat: route to list all contacts of the logged in user created

--- src/controllers/contacts.controller.ts
+++ src/controllers/contacts.controller.ts
@@ -3,33 +3,40 @@ import { ICreateContact, IUpdateContact } from "../interfaces/contacts";
 import ContactByIdService from "../services/contacts/ContactById.service";
 import createContactService from "../services/contacts/createContact.service";
 import deleteContactService from "../services/contacts/deleteContact.service";
+import listUsercontactsService from "../services/contacts/listUsercontacts.service";
 import updateContactService from "../services/contacts/updateContact.service";
 
 const createContactController = async (req: Request, res: Response) => {
     const contactData: ICreateContact = req.body
     const userId: string = req.user.id 
-    const newUser = await createContactService(contactData, userId)
-    return res.status(201).json(newUser)
+    const newContact = await createContactService(contactData, userId)
+    return res.status(201).json(newContact)
+}
+
+const listUsercontactsController = async (req: Request, res: Response) => {
+    const userId: string = req.user.id 
+    const listContact = await listUsercontactsService(userId)
+    return res.status(200).json(listContact)
 }
 
 const ContactByIdController = async (req: Request, res: Response) => {
     const contactId: string = req.params.id
-    const infoUser = await ContactByIdService(contactId)
-    return res.status(200).json(infoUser)
+    const infoContact = await ContactByIdService(contactId)
+    return res.status(200).json(infoContact)
 }
 
 const updateContactController = async (req: Request, res: Response) => {
     const contactData: IUpdateContact = req.body
     const userId: string = req.user.id 
     const contactId: string = req.params.id
-    const updateUser = await updateContactService(contactData, userId, contactId)
-    return res.status(200).json(updateUser)
+    const updateContact = await updateContactService(contactData, userId, contactId)
+    return res.status(200).json(updateContact)
 }
 
 const deleteContactController = async (req: Request, res: Response) => {
     const contactId: string = req.params.id
-    const deleteUser = await deleteContactService(contactId)
-    return res.status(204).json(deleteUser)
+    const deleteContact = await deleteContactService(contactId)
+    return res.status(204).json(deleteContact)
 }
 
-export {createContactController, updateContactController, ContactByIdController, deleteContactController}
+export {createContactController, updateContactController, ContactByIdController, deleteContactController, listUsercontactsController}
--- src/routers/contacts.routes.ts
+++ src/routers/contacts.routes.ts
@@ -1,5 +1,5 @@
 import {Router} from "express"
-import { ContactByIdController, createContactController, deleteContactController, updateContactController } from "../controllers/contacts.controller"
+import { ContactByIdController, createContactController, deleteContactController, listUsercontactsController, updateContactController } from "../controllers/contacts.controller"
 import ensureAuthMiddleware from "../middlewares/ensureAuth.middleware"
 import ensureContactIdExistMiddleware from "../middlewares/ensureContactIdExist.middleware"
 import ensureContactOwnerMiddleware from "../middlewares/ensureContactOwner.middleware"
@@ -9,6 +9,7 @@ import { createContactSchema, updateContactSchema } from "../schemas/contact.sch
 const contactRoutes = Router()
 
 contactRoutes.post("", ensureAuthMiddleware, ensureDataIsValidMiddleware(createContactSchema), createContactController)
+contactRoutes.get("", ensureAuthMiddleware, listUsercontactsController)
 contactRoutes.get("/:id", ensureAuthMiddleware, ensureContactIdExistMiddleware, ensureContactOwnerMiddleware, ContactByIdController)
 contactRoutes.patch("/:id", ensureAuthMiddleware, ensureContactIdExistMiddleware, ensureContactOwnerMiddleware, ensureDataIsValidMiddleware(updateContactSchema), updateContactController)
 contactRoutes.delete("/:id", ensureAuthMiddleware, ensureContactIdExistMiddleware, ensureContactOwnerMiddleware, deleteContactController)
